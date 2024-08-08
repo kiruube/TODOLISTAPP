badd313ebd6e220ceb56dcd2827b57203bf256cf
Kanza Jovita <kanzajovita@gmail.com>
2024-08-08 13:05:13 +0300

    added an add task feature

--- ToDo/toDoApp/views.py
+++ ToDo/toDoApp/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import Task
 
 # Create your views here.
+def add_task(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            Task.objects.create(title=title)
+        return redirect('index')
+    return render(request, 'add_task.html')
