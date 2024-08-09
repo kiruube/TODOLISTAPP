391ca5786351a9c5b1f141213dc357134ca96252
Daphine Nambafu <daphinenambafu@gmail.com>
2024-08-09 08:56:18 +0300

    added the url for index.html

--- ToDo/toDoApp/views.py
+++ ToDo/toDoApp/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render,redirect
 from .models import Task
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    
 def add_task(request):
     if request.method == 'POST':
         title = request.POST.get('title')
